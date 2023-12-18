80701797f57e19da67e8a539990ea5f863a4bca4
Ahmet Elbüz <elbuzahmet@gmail.com>
2023-12-18 20:22:22 +0300

    1 ile 100 arası asal sayılar

--- AsalSayi/src/Main.java
+++ AsalSayi/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i =2; i<=100; i++){
+        for (int i =2; i<=100; i++){ // ilk sayımızı 1 den 100 e kadar döndürüyoruz
             boolean asal = true;
-            for (int j =2; j<i; j++){
-                if(i %j==0){
+            for (int j =2; j<i; j++){ // ikinci sayımızı i den küçük yapıyoruz çünkü j=i zaten kendisi asal
+                if(i %j==0){ // eğer i nin j ye bölümünden kalan 0 ise asal değildir boolen false olur
                     asal = false;
                     break;
                 }
 
             }
-            if (asal){
+            if (asal){ // asal değişkeni true gelirse ekrana yazdırır
                 System.out.print(i + " ");
             }
         }
